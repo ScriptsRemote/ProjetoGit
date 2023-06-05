b164c5416a6dbff54e2c4d182862b88564c2a671
CHRISTHIAN SANTANA CUNHA <96967609+ScriptsRemote@users.noreply.github.com>
2023-06-05 20:17:49 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,21 +1,9 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash
-import dash.html as html
-import dash.dcc as dcc
-from dash.dependencies import Input, Output
+import streamlit as st
 import plotly.express as px
 import pandas as pd
 
 # Carrega os dados do CSV
 df = pd.read_csv('dados.csv')
-
-# Cria a aplicação Dash
-app = Dash(__name__)
 
 # Define os estados disponíveis no arquivo CSV
 estados_disponiveis = df['Estado'].unique()
@@ -24,50 +12,19 @@ estados_disponiveis = df['Estado'].unique()
 meses_disponiveis = pd.date_range(start='2023-01-01', end='2023-12-31', freq='M').strftime('%b')
 
 # Layout da aplicação
-app.layout = html.Div([
-    html.H1('Dashboard de Dados'),
-    html.Div([
-        html.Div([
-            html.H3('Gráfico de Temperatura'),
-            dcc.Dropdown(
-                id='estado-dropdown',
-                options=[{'label': estado, 'value': estado} for estado in estados_disponiveis],
-                value=estados_disponiveis[0]
-            ),
-            dcc.Graph(id='temperatura-graph')
-        ], className='six columns'),
+st.title('Dashboard de Dados')
 
-        html.Div([
-            html.H3('Gráfico de NDVI'),
-            dcc.Dropdown(
-                id='mes-dropdown',
-                options=[{'label': mes, 'value': mes} for mes in meses_disponiveis],
-                value=meses_disponiveis[0]
-            ),
-            dcc.Graph(id='ndvi-graph')
-        ], className='six columns')
-    ], className='row')
-])
+# Gráfico de Temperatura
+st.header('Gráfico de Temperatura')
+estado = st.selectbox('Selecione um estado', estados_disponiveis)
+data_temperatura = df[df['Estado'] == estado]
+fig_temperatura = px.line(data_temperatura, x='Data', y='Temperatura', title='Temperatura por Data', symbol='Temperatura')
+st.plotly_chart(fig_temperatura)
 
-# Callback para atualizar o gráfico de temperatura
-@app.callback(
-    Output('temperatura-graph', 'figure'),
-    [Input('estado-dropdown', 'value')]
-)
-def update_temperatura_graph(estado):
-    data = df[df['Estado'] == estado]
-    fig = px.line(data, x='Data', y='Temperatura', title='Temperatura por Data', symbol='Temperatura')
-    return fig
+# Gráfico de NDVI
+st.header('Gráfico de NDVI')
+mes = st.selectbox('Selecione um mês', meses_disponiveis)
+data_ndvi = df[df['Data'].str.startswith(mes)]
+fig_ndvi = px.line(data_ndvi, x='Data', y='NDVI', title='NDVI por Data', symbol='NDVI')
+st.plotly_chart(fig_ndvi)
 
-# Callback para atualizar o gráfico de NDVI
-@app.callback(
-    Output('ndvi-graph', 'figure'),
-    [Input('mes-dropdown', 'value')]
-)
-def update_ndvi_graph(mes):
-    data = df[df['Data'].str.startswith(mes)]
-    fig = px.line(data, x='Data', y='NDVI', title='NDVI por Data', symbol='NDVI')
-    return fig
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
